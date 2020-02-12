360737bffe1947cdfca85cf4c108eaf269b5000e
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2020-02-12 17:04:40 +0100

    first nav implementation (not finished)

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 console.log("app.js is loaded")
 const d = document
+const w = window
 
 
-// Variables declaration - General scope
+// -----------------------------  Variables declaration - General scope  -----------------------------
+
+// FadeUp
 const fade_els = d.querySelectorAll(".fade")
 let autoWritingCount = 0
 
+// DynamicNav
+const mainContent = d.querySelector("main")
+const nav = d.querySelector("nav")
+const hamburgerBtn = d.querySelector(".hamburger_btn")
+const header = d.querySelector("header")
+const navWidth = 20
+let nav_isShowed = false
+let headerObserver_switch = true
 
-//Functions Creation
 
-// Fade effect
+// -----------------------------  Functions Creation  -----------------------------
+
+
+// FadeUp
 const fadeEffect = () => {
   fade_els.forEach(e => {
     if((e.getBoundingClientRect().top - (window.innerHeight - (15/100)*window.innerHeight) <= 0)){
@@ -20,17 +33,35 @@ const fadeEffect = () => {
   })
 }
 
-// Text auto writing
-const autoWriting = (text, div, speed) => {
-  setInterval(() => {
-    if(autoWritingCount < text.length){
-      div.innerHTML += text[autoWritingCount]
-      autoWritingCount++
-    }
-  }, speed)
+// DynamicNav
+const menuShow = () => {
+  if(!nav_isShowed){
+    nav.style.width = navWidth + "%"
+    mainContent.style.width = (100 - navWidth) + "%"
+    mainContent.style.marginLeft = navWidth + "%"
+    nav_isShowed = true
+  } else {
+    mainContent.style.width = 100 + "%"
+    mainContent.style.marginLeft = 0 + "%"
+    nav.style.width = 0 + "%"
+    nav_isShowed = false
+  }
 }
 
+const headerObeserverOptions = {
+  rootMargin: '0px'
+}
+const headerObeserver = new IntersectionObserver(() => {
+  if(headerObserver_switch && w.scrollY < w.innerHeight){
+    hamburgerBtn.style.color = "#D3D3D3"
+    headerObserver_switch = false
+  } else {
+    hamburgerBtn.style.color = "#FF5733"
+    headerObserver_switch = true
+  }
+}, headerObeserverOptions)
+headerObeserver.observe(header)
 
-// Functions Calls - Event Listeners
+
+// -----------------------------  Functions Calls - Event Listeners  -----------------------------
 window.addEventListener("scroll", fadeEffect)
-// autoWriting("Entreprenor - Web developper", d.querySelector(".job"), 100)
